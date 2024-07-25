67e211dc6c245e8d41de7fa3e1392329d33c3951
kinsonpoon <kinson.poon@mdland.com>
2024-07-25 12:42:51 -0400

    I think is done

--- my-app/src/scenes/home.tsx
+++ my-app/src/scenes/home.tsx
@@ -18,6 +18,17 @@ import Chip from '@mui/material/Chip';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import InstagramIcon from '@mui/icons-material/Instagram';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CloseIcon from '@mui/icons-material/Close';
+import {
+    Accordion, AccordionDetails,
+    AccordionSummary,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    FormLabel, Snackbar
+} from "@mui/material";
 
 interface Props {
     /**
@@ -38,6 +49,18 @@ export const openInNewTab = (url: string): void => {
 export default function Home(props: Props) {
     const {window} = props;
     const [mobileOpen, setMobileOpen] = React.useState(false);
+    const [open, setOpen] = React.useState(false);
+    const handleClick = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
 
     const handleDrawerToggle = () => {
         setMobileOpen((prevState) => !prevState);
@@ -52,13 +75,26 @@ export default function Home(props: Props) {
             <List>
                 {navItems.map((item) => (
                     <ListItem key={item} disablePadding>
-                        <ListItemButton sx={{textAlign: 'center'}}>
+                        <ListItemButton sx={{textAlign: 'center'}} onClick={handleClick}>
                             <ListItemText primary={item}/>
                         </ListItemButton>
                     </ListItem>
                 ))}
             </List>
         </Box>
+    );
+
+    const action = (
+        <React.Fragment>
+            <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleClose}
+            >
+                <CloseIcon fontSize="small" />
+            </IconButton>
+        </React.Fragment>
     );
 
     const container = window !== undefined ? () => window().document.body : undefined;
@@ -86,7 +122,7 @@ export default function Home(props: Props) {
                     </Typography>
                     <Box sx={{display: {xs: 'none', sm: 'block'}}}>
                         {navItems.map((item) => (
-                            <Button key={item} sx={{color: '#fff'}}>
+                            <Button key={item} sx={{color: '#fff'}} onClick={handleClick}>
                                 {item}
                             </Button>
                         ))}
@@ -110,6 +146,13 @@ export default function Home(props: Props) {
                     {drawer}
                 </Drawer>
             </nav>
+            <Snackbar
+                open={open}
+                autoHideDuration={2000}
+                onClose={handleClose}
+                message="Hi There! I am Kinson. :)"
+                action={action}
+            />
             <Box component="main" p={{xs: 1, sm: 2, md: 3}} width={'100%'}>
                 <Toolbar/>
                 <Box>
@@ -133,14 +176,47 @@ export default function Home(props: Props) {
                             <Typography>
                                 Skills:
                             </Typography>
-                            <Stack direction="row" spacing={1} ml={1}>
+                            <Stack direction="row" spacing={1} ml={1} useFlexGap flexWrap="wrap">
                                 <Chip variant="outlined" label="React" size="small" />
                                 <Chip variant="outlined" label="Angular" size="small" />
                                 <Chip variant="outlined" label="TS" size="small" />
                                 <Chip variant="outlined" label="JAVA" size="small" />
                                 <Chip variant="outlined" label="AWS" size="small" />
-                                <Chip variant="outlined" label="CICD" size="small" />
+                                <Chip variant="outlined" label="FirseBase" size="small" />
+                                <Chip variant="outlined" label="Lambda" size="small" />
+                                <Chip variant="outlined" label="SpringBoot" size="small" />
                             </Stack>
+                        </Box>
+                        <Box>
+                            <FormControl sx={{ mt: 2 }} component="fieldset" variant="standard">
+                                <FormLabel component="legend">Languages</FormLabel>
+                                <FormGroup sx={{flexDirection:{xs: 'column', sm: 'row', md: 'row'}}}>
+                                    <FormControlLabel
+                                        control={
+                                            <Checkbox checked={true} name="English" />
+                                        }
+                                        label="English"
+                                    />
+                                    <FormControlLabel
+                                        control={
+                                            <Checkbox checked={true} name="Chinese" />
+                                        }
+                                        label="Chinese(Mandarin & Cantonese)"
+                                    />
+                                    <FormControlLabel
+                                        control={
+                                            <Checkbox checked={true} name="Japanese" />
+                                        }
+                                        label="Japanese"
+                                    />
+                                </FormGroup>
+                            </FormControl>
+                        </Box>
+                        <Box>
+                            <img src={require('../assets/hong-kong.png')} alt="img2"
+                                 style={{width: '50px', height: 'auto'}}/>
+                            <img src={require('../assets/canada.png')} alt="img3"
+                                 style={{width: '50px', height: 'auto', marginLeft: '10px'}}/>
                         </Box>
                     </Box>
                     <Box justifyContent={'flex-end'} width={{sm: '100%', md: '40%'}} ml={'auto'}>
@@ -148,11 +224,65 @@ export default function Home(props: Props) {
                              style={{width: '100%', height: 'auto'}}/>
                     </Box>
                 </Box>
+                <Box mt={2}>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            Projects
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>- ClimbFUnity Web app</Typography>
+                            <Typography>- MdLand SDOH Web app</Typography>
+                            <Typography>- MdLand IPH Web app</Typography>
+                            <Typography>- Club Monaco HK eshop</Typography>
+                            <Typography>- Nike and Sacai HK event site</Typography>
+                            <Typography>- Varadise DWSS Web app</Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2-content"
+                            id="panel2-header"
+                        >
+                            Experience
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>Senior Software Developer---MDLand,Canada</Typography>
+                            <Typography>Software Developer---MDLand,Canada</Typography>
+                            <Typography>Full-Stack Developer---Digidumpling,Hong Kong</Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                    <Accordion>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="panel2-content"
+                            id="panel3-header"
+                        >
+                            Outdoor Climbing Experience
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Typography>Squmaish-BC,Canada</Typography>
+                            <Typography>Niagara Glen-ON,Canada</Typography>
+                            <Typography>Everywhere in Hong Kong</Typography>
+                            <Typography>Mitake-Japan</Typography>
+                            <Typography>Long Dong-Taiwan</Typography>
+                            <Typography>Guanziling-Taiwan</Typography>
+                            <Typography>El Chorro-Spain</Typography>
+                            <Typography>Green Climbers Home-Laos</Typography>
+                            <Typography>Krabi-Thailand</Typography>
+                            <Typography>Nam Pha Pa Yai-Thailand</Typography>
+                        </AccordionDetails>
+                    </Accordion>
+                </Box>
                 <Box mt={2} display={'flex'} justifyContent={'center'} alignItems={'center'}>
                     <Stack direction="row" spacing={1}>
-                        <Chip icon={<LinkedInIcon/>} label="LinkedIn" clickable onClick={()=>openInNewTab('www.google.com')}/>
-                        <Chip icon={<GitHubIcon/>} label="GitHub" clickable onClick={()=>openInNewTab('www.google.com')}/>
-                        <Chip icon={<InstagramIcon/>} label="Instagram" clickable onClick={()=>openInNewTab('www.google.com')}/>
+                        <Chip icon={<LinkedInIcon/>} label="LinkedIn" clickable onClick={()=>openInNewTab('LinkedIn.com/in/king-hin-poon-aa7858134')}/>
+                        <Chip icon={<GitHubIcon/>} label="GitHub" clickable onClick={()=>openInNewTab('https://github.com/kinsonpoon')}/>
+                        <Chip icon={<InstagramIcon/>} label="Instagram" clickable onClick={()=>openInNewTab('https://www.instagram.com/o0kinson0o/?hl=en')}/>
                     </Stack>
                 </Box>
             </Box>
